e0b4e56d4619ba786d5def721fd0eb24338058eb
alessiaskia <a.scaccia@interface3.be>
2020-12-14 09:33:29 +0100

    finished static part

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ const app = document.getElementById('app');
 
 // every files in "static" folder can be used directly like that
 // app.innerHTML += '<img src="images/kitten.jpg" style="width:100px;" />';
+// create section recent field only
+const recent = '<button id="recent">Recent films only</button>';
 
+// create card for each movie with image, assign numeric ID
 let card = '';
 let container = '<section class="flex-wrap">';
 let movieId = 0;
 
 for (const movie of movies) {
-  // create card for each movie with image, assign numeric ID
   card = `
-      <div class="card" id="${movieId}">`;
+        <div class="card" id="${movieId}">`;
   // verify if poster available
   if (movie.img) {
     card += `<img src="images/${movie.imdb}.jpg" alt="poster ${movie.title}" />`;
   } else {
-    card += `<div class="card-no-poster"><h3 class="card-title">${movie.title}</h3></div>`;
+    card += `<div class="card empty"><h3>${movie.title}</h3></div>`;
   }
   card += '</div>';
   // increase id
@@ -28,4 +30,10 @@ for (const movie of movies) {
 }
 container += '</div>';
 // show in 'app'
-app.innerHTML = `${container}</section>`;
+app.innerHTML = `${recent + container}</section>`;
+
+document.body.addEventListener('click', (e) => {
+  if (e.target.matches('#recent')) {
+    console.log('ok');
+  }
+});
